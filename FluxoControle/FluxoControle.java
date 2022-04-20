a36b4ff444936d2115bf5a7639aced3875b87ce0
Carlos Henrique <92608686+carloschac@users.noreply.github.com>
2022-04-20 16:53:56 -0300

    commit

---
+++ FluxoControle/FluxoControle.java
@@ -0,0 +1,19 @@
+public class FluxoControle {
+  public static void main(String[] args) {
+
+    int idade = 15;
+
+    if (idade >= 18 && idade < 70) {
+      System.out.println("Maior idade");
+    }
+
+    else if (idade >= 70) {
+      System.out.println("Terceira idade");
+    }
+
+    else {
+      System.out.println("Menor idade");
+    }
+
+  }
+}
