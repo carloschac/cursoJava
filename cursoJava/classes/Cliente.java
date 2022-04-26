d672140a2d651d44c0760db80f223774e0e6829f
Carlos Henrique <92608686+carloschac@users.noreply.github.com>
2022-04-26 15:33:12 -0300

    commit

---
+++ cursoJava/classes/Cliente.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class Cliente {
+
+  String nome;
+
+  public Cliente(String nome, byte idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+  byte idade;
+
+}
---
+++ cursoJava/classes/Conta.java
@@ -0,0 +1,7 @@
+package classes;
+
+public class Conta {
+  int numero;
+  double saldo;
+  public Object cliente;
+}
